e712dd054e20a74090b1c35c7a6bfc849bbeb587
Iqra Rahman <rahmaniqra697@gmail.com>
2025-09-26 17:24:30 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+import { connect } from 'mongoose';
+import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js';
+import userRouter from './routes/userRoute.js';
+
+const app = express();
+const port = process.env.PORT || 5000;
+connectDB(); 
+
+const allowedOrigins = ['http://localhost:5173']
+
+app.use(cors({origin: allowedOrigins, credentials: true}));
+app.use(express.json());
+app.use(cookieParser());
+
+//API Endpoints
+app.get('/', (req, res)=> res.send('API is working'));
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
+
+app.listen(port, ()=> console.log(`server started on port ${port}`));
